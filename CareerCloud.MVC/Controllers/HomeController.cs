ffa52ee1d164c80086a3c12d0ced147d2547eeb2
Hassan Mostafa <Hassan.a.mostafa@gmail.com>
2017-10-19 11:51:17 -0400

    About and Contact pages added. Home page modified

--- CareerCloud.MVC/Controllers/HomeController.cs
+++ CareerCloud.MVC/Controllers/HomeController.cs
@@ -15,14 +15,14 @@ namespace CareerCloud.MVC.Controllers
 
         public ActionResult About()
         {
-            ViewBag.Message = "Your application description page.";
+            ViewBag.Message = "Career Cloud";
 
             return View();
         }
 
         public ActionResult Contact()
         {
-            ViewBag.Message = "Your contact page.";
+            ViewBag.Message = "Career Cloud contacts";
 
             return View();
         }
